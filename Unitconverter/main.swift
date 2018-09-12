c9fcf318b5e00b88f14db559ef62ae86e28056c9
Dongmin.yoon <ehdals8965@naver.com>
2018-09-12 21:16:50 +0900

    인치 길이 변환과 예외 처리

--- Unitconverter/main.swift
+++ Unitconverter/main.swift
@@ -8,28 +8,88 @@
 
 import Foundation
 
-let convertValue : Float = 100
+let convertcmTomValue : Float = 100
+let convertcmToinchValue : Float = 0.393
 
-func centimeterTometer (originNumber : String){
-    let converNumber : Float = Float(originNumber)! / convertValue
-    print("\(converNumber)m")
+func centimeterTometer (originNumber : Float) -> Float{                                 // cm --> m
+    let converNumber : Float = originNumber / convertcmTomValue
+    
+    return converNumber
 }
 
-func meterTocentimeter (originNUmber : String){
-    let convertNumber : Float = Float(originNUmber)! * convertValue
-    print("\(convertNumber)cm")
+func meterTocentimeter (originNumber : Float) -> Float{                                 // m --> cm
+    let convertNumber : Float = originNumber * convertcmTomValue
+    
+    return convertNumber
 }
 
-var number : String = readLine()!
+func centimeterToinch (originNumber : Float) -> Float{                                  // cm --> inch
+    let convertNumber : Float = originNumber * convertcmToinchValue
 
-var arr = number.components(separatedBy: " ")
+    return convertNumber
+}
 
-if arr[1] == "cm"{
-    centimeterTometer(originNumber: arr[0])
+func inchTocentimeter (originNumber : Float) -> Float{                                  // inch --> cm
+    let convertNumber : Float = originNumber / convertcmToinchValue
+    
+    return convertNumber
 }
-else if arr[1] == "m"{
-    meterTocentimeter(originNUmber: arr[0])
+
+func meterToinch (originNumber : Float) -> Float{                                       // m --> inch
+    let convertNumber = centimeterToinch(originNumber: meterTocentimeter(originNumber: originNumber))
+    
+    return convertNumber
 }
+
+func inchTometer (originNumber : Float) -> Float{                                       // inch --> m
+    let convertNumber = centimeterTometer(originNumber: inchTocentimeter(originNumber: originNumber))
+    
+    return convertNumber
+}
+
+while true{
+    
+    let number : String = readLine()!
+
+    var arr = number.components(separatedBy: " ")
+
+    if arr[1] == "inch" && arr[1] == "cm"{
+        let convertNumber : Float = inchTocentimeter(originNumber: Float(arr[0])!)
+        print("\(convertNumber)cm")
+        break
+    }
+    else if arr[1] == "cm" && arr[2] == "inch"{
+        let convertNumber : Float = centimeterToinch(originNumber: Float(arr[0])!)
+        print("\(convertNumber)inch")
+        break
+    }
+    else if arr[1] == "inch" && arr[2] == "m"{
+        let convertNumber : Float = inchTometer(originNumber: Float(arr[0])!)
+        print("\(convertNumber)m")
+        break
+    }
+    else if arr[1] == "m" && arr[2] == "inch"{
+        let convertNumber : Float = meterToinch(originNumber: Float(arr[0])!)
+        print("\(convertNumber)inch")
+        break
+    }
+    else if arr[1] == "cm" {
+        let convertNumber : Float = centimeterTometer(originNumber: Float(arr[0])!)
+        print("\(convertNumber)m")
+        break
+    }
+    else if arr[1] == "m"{
+        let convertNumber : Float = meterTocentimeter(originNumber: Float(arr[0])!)
+        print("\(convertNumber)cm")
+        break
+    }
+    else{
+        print("지원하지 않는 단위입니다.")
+    }
+}
+
+
+
 
 
 
